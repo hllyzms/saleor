625bcc2e790a2c327978132fd50c39c38696a1ac
Pacu2 <filip.m.kucharczyk@gmail.com>
2018-10-04 16:07:00 +0200

    Improve docstrings, add validation for some of the mutaitons

--- tests/conftest.py
+++ tests/conftest.py
@@ -111,6 +111,7 @@ def graphql_address_data():
         'city': 'New York',
         'countryArea': ''}
 
+
 @pytest.fixture
 def customer_user(address):  # pylint: disable=W0613
     default_address = address.get_copy()
@@ -475,6 +476,7 @@ def payment_preauth(order_with_lines):
         total=order_with_lines.total.gross.amount,
         tax=order_with_lines.total.tax.amount)
 
+
 @pytest.fixture()
 def payment_method_txn_preauth(order_with_lines, payment_method_dummy):
     order = order_with_lines
@@ -489,6 +491,7 @@ def payment_method_txn_preauth(order_with_lines, payment_method_dummy):
         gateway_response={},
         is_success=True)
     return payment
+
 
 @pytest.fixture()
 def payment_method_txn_charged(order_with_lines, payment_method_dummy):
@@ -507,6 +510,7 @@ def payment_method_txn_charged(order_with_lines, payment_method_dummy):
         is_success=True)
     return payment
 
+
 @pytest.fixture()
 def payment_method_txn_refunded(order_with_lines, payment_method_dummy):
     order = order_with_lines
@@ -524,6 +528,7 @@ def payment_method_txn_refunded(order_with_lines, payment_method_dummy):
         gateway_response={},
         is_success=True)
     return payment
+
 
 @pytest.fixture()
 def payment_confirmed(order_with_lines):
@@ -769,6 +774,7 @@ def product_translation_fr(product):
     return ProductTranslation.objects.create(
         language_code='fr', product=product, name='French name',
         description='French description')
+
 
 @pytest.fixture
 def payment_method_dummy(db):
